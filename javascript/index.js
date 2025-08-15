52b4cf811b04f94d42de8e7a58408964b41d025a
Prakash Singh Chauhan <pchauhan@Prakashs-MacBook-Pro.local>
2025-08-15 22:18:37 +0530

    feat: add initial classic project

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  document.getElementById("msg--text").textContent = "Hello World !!";
+  document.getElementById("msg").style.display = "block";
+}
+
+function closeMessage() {
+  document.getElementById("msg").style.display = "none";
+}
